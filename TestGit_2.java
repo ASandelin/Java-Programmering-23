c96dc136a1e584c603718a4cedf9d4ca1718836e
ArturSandelin <artursandelin@gmail.com>
2023-09-07 19:32:35 +0200

    Test_03

--- TestGit_2.java
+++ TestGit_2.java
@@ -1,5 +1,7 @@
 package Java-Programmering-23;
 
 public class TestGit_2 {
-    
+    public static void main(String[] args) {
+        
+    }
 }
--- TestGit_3.java
+++ TestGit_3.java
@@ -1,5 +1,7 @@
 package Java-Programmering-23;
 
 public class TestGit_3 {
-    
+    public static void main(String[] args) {
+        
+    }
 }
